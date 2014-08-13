6febf048d821290ffceefb8145d7f0bfbfd339df
Thomas Rehn <thomas.rehn@initos.com>
2014-08-13 09:30:12 +0200

    unmerged fix lp:~openerp-dev/openobject-addons/7.0-opw-593375-rgo

--- addons/sale/wizard/sale_make_invoice_advance.py
+++ addons/sale/wizard/sale_make_invoice_advance.py
@@ -77,35 +77,38 @@ class sale_advance_payment_inv(osv.osv_memory):
         sale_ids = context.get('active_ids', [])
 
         result = []
+        inv_lines = []
         for sale in sale_obj.browse(cr, uid, sale_ids, context=context):
-            val = inv_line_obj.product_id_change(cr, uid, [], wizard.product_id.id,
-                    uom_id=False, partner_id=sale.partner_id.id, fposition_id=sale.fiscal_position.id)
-            res = val['value']
-
-            # determine and check income account
-            if not wizard.product_id.id :
-                prop = ir_property_obj.get(cr, uid,
-                            'property_account_income_categ', 'product.category', context=context)
-                prop_id = prop and prop.id or False
-                account_id = fiscal_obj.map_account(cr, uid, sale.fiscal_position or False, prop_id)
-                if not account_id:
-                    raise osv.except_osv(_('Configuration Error!'),
-                            _('There is no income account defined as global property.'))
-                res['account_id'] = account_id
-            if not res.get('account_id'):
-                raise osv.except_osv(_('Configuration Error!'),
-                        _('There is no income account defined for this product: "%s" (id:%d).') % \
-                            (wizard.product_id.name, wizard.product_id.id,))
-
-            # determine invoice amount
             if wizard.amount <= 0.00:
                 raise osv.except_osv(_('Incorrect Data'),
                     _('The value of Advance Amount must be positive.'))
             if wizard.advance_payment_method == 'percentage':
-                inv_amount = sale.amount_total * wizard.amount / 100
-                if not res.get('name'):
-                    res['name'] = _("Advance of %s %%") % (wizard.amount)
+                for line in sale.order_line:
+                    inv_line_vals = self.pool.get('sale.order.line')._prepare_order_line_invoice_line(cr, uid, line, False, context=context)
+                    inv_line_vals['price_unit'] = inv_line_vals['price_unit'] * wizard.amount / 100
+                    inv_line_vals['name'] += "\n(" + _("Advance of %s %%") % (wizard.amount) + ")"
+                    inv_lines.append((0, 0, inv_line_vals))
             else:
+                val = inv_line_obj.product_id_change(cr, uid, [], wizard.product_id.id,
+                        uom_id=False, partner_id=sale.partner_id.id, fposition_id=sale.fiscal_position.id)
+                res = val['value']
+
+            # determine and check income account
+                if not wizard.product_id.id :
+                    prop = ir_property_obj.get(cr, uid,
+                                'property_account_income_categ', 'product.category', context=context)
+                    prop_id = prop and prop.id or False
+                    account_id = fiscal_obj.map_account(cr, uid, sale.fiscal_position or False, prop_id)
+                    if not account_id:
+                        raise osv.except_osv(_('Configuration Error!'),
+                                _('There is no income account defined as global property.'))
+                    res['account_id'] = account_id
+                if not res.get('account_id'):
+                    raise osv.except_osv(_('Configuration Error!'),
+                            _('There is no income account defined for this product: "%s" (id:%d).') % \
+                                (wizard.product_id.name, wizard.product_id.id,))
+
+                # determine invoice amount
                 inv_amount = wizard.amount
                 if not res.get('name'):
                     #TODO: should find a way to call formatLang() from rml_parse
@@ -115,25 +118,25 @@ class sale_advance_payment_inv(osv.osv_memory):
                     else:
                         res['name'] = _("Advance of %s %s") % (symbol, inv_amount)
 
-            # determine taxes
-            if res.get('invoice_line_tax_id'):
-                res['invoice_line_tax_id'] = [(6, 0, res.get('invoice_line_tax_id'))]
-            else:
-                res['invoice_line_tax_id'] = False
+                # determine taxes
+                if res.get('invoice_line_tax_id'):
+                    res['invoice_line_tax_id'] = [(6, 0, res.get('invoice_line_tax_id'))]
+                else:
+                    res['invoice_line_tax_id'] = False
 
-            # create the invoice
-            inv_line_values = {
-                'name': res.get('name'),
-                'origin': sale.name,
-                'account_id': res['account_id'],
-                'price_unit': inv_amount,
-                'quantity': wizard.qtty or 1.0,
-                'discount': False,
-                'uos_id': res.get('uos_id', False),
-                'product_id': wizard.product_id.id,
-                'invoice_line_tax_id': res.get('invoice_line_tax_id'),
-                'account_analytic_id': sale.project_id.id or False,
-            }
+                # create the invoice
+                inv_line_values = {
+                    'name': res.get('name'),
+                    'origin': sale.name,
+                    'account_id': res['account_id'],
+                    'price_unit': inv_amount,
+                    'quantity': wizard.qtty or 1.0,
+                    'discount': False,
+                    'uos_id': res.get('uos_id', False),
+                    'product_id': wizard.product_id.id,
+                    'invoice_line_tax_id': res.get('invoice_line_tax_id'),
+                    'account_analytic_id': sale.project_id.id or False,
+                }
             inv_values = {
                 'name': sale.client_order_ref or sale.name,
                 'origin': sale.name,
@@ -141,7 +144,7 @@ class sale_advance_payment_inv(osv.osv_memory):
                 'reference': False,
                 'account_id': sale.partner_id.property_account_receivable.id,
                 'partner_id': sale.partner_invoice_id.id,
-                'invoice_line': [(0, 0, inv_line_values)],
+                'invoice_line': inv_lines or [(0, 0, inv_line_values)],
                 'currency_id': sale.pricelist_id.currency_id.id,
                 'comment': '',
                 'payment_term': sale.payment_term.id,
