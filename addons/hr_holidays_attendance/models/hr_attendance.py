c6f89d5024a8c1a12b2c37150671aa09ae2e7097
Ruben Ortlam <ruben.ortlam@initos.com>
2023-12-18 11:23:27 +0100

    [FIX] Fix attendance overtime calculation ignoring public holidays

--- addons/hr_holidays_attendance/models/hr_attendance.py
+++ addons/hr_holidays_attendance/models/hr_attendance.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
-from odoo.osv.expression import AND
+from odoo.osv.expression import AND, OR
 
 
 class HrAttendance(models.Model):
@@ -10,4 +10,10 @@ class HrAttendance(models.Model):
 
     def _get_overtime_leave_domain(self):
         domain = super()._get_overtime_leave_domain()
-        return AND([domain, [('holiday_id.holiday_status_id.time_type', '=', 'leave')]])
+        return AND([
+            domain,
+            OR([
+                [("holiday_id", "=", False)],
+                [("holiday_id.holiday_status_id.time_type", "=", "leave")],
+            ]),
+        ])
