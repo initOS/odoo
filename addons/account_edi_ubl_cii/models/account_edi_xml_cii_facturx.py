c185f73f286446ba0be92f8cf3e546632ce62acb
fkantelberg <florian.kantelberg@initos.com>
2025-07-01 09:11:08 +0200

    [FIX] account_edi_ubl_cii: Fallback to instrument not defined

--- addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_cii_facturx.py
@@ -34,7 +34,7 @@ class AccountEdiXmlCII(models.AbstractModel):
 
     def _export_invoice_constraints(self, invoice, vals):
         constraints = self._invoice_constraints_common(invoice)
-        if invoice.move_type in ('out_invoice', 'out_refund'):
+        if invoice.move_type == 'out_invoice':
             # [BR-DE-1] An Invoice must contain information on "PAYMENT INSTRUCTIONS" (BG-16)
             # first check that a partner_bank_id exists, then check that there is an account number
             # FIX: Validation for these fields also happen on credit notes
